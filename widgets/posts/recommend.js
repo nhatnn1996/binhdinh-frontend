1b90b74e20982716521471b51e048e76f2c47113
Nhatnn <nhatnn@galaxyplay.vn>
2021-04-23 09:58:11 +0700

    [update]

--- widgets/posts/recommend.js
+++ widgets/posts/recommend.js
@@ -23,14 +23,27 @@ const Post = () => {
   return (
     <Link href="">
       <div className="w-1/3 mr-2">
-        <img
-          className="post-main-image mt-2"
-          src="https://anhdepfree.com/wp-content/uploads/2018/08/hinh-nen-dep-nhat-2018.jpg"
-          layout="fill"
-        />
-        <div className="text-base font-bold text-gray-700	mt-2 hover:text-blue-700">
+        <div className="box mt-2">
+          <img
+            className="box-image"
+            src="https://anhdepfree.com/wp-content/uploads/2018/08/hinh-nen-dep-nhat-2018.jpg"
+            layout="fill"
+          />
+        </div>
+        <div className="text-base font-bold text-gray-700	mt-2 hoer:text-blue-700 pointer">
           Phương án đấu nối cấp nước vào hộ gia đình của Công trình cấp nước sinh hoạt huyện Phù Cát
         </div>
+        <style global jsx>{`
+          .box {
+            overflow: hidden;
+          }
+          .box-image:hover {
+            transform: scale(1.1) rotate(1deg)
+          }
+          .box-image {
+            transition: transform .3s ease-in-out;
+          }
+        `}</style>
       </div>
     </Link>
   );
--- widgets/video/index.js
+++ widgets/video/index.js
@@ -5,12 +5,61 @@ const Video = () => {
     <div className="post-main py-5 mt-5 flex">
       <div className="">
         <div className="font-bold text-blue-600 text-md">TIN NỔI BẬT</div>
-        <div className="mt-4 flex">
-          
+        <div className="flex">
+          <div className="w-3/4 pr-3">
+            <video className="video-main" src="/videos/Fintech_video.mp4" controls />
+            <div className="overlay"></div>
+            <div className="font-bold mt-6">Title Video nè </div>
+          </div>
+          <div className="w-1/4">
+            <div className="flex box-image-video">
+              <img src="https://cdn.yeudulich.com/940x630/media/attraction/attraction/e5/df/47b9-4bea-4849-b339-f8c17c8aecfa.jpg" alt="" />
+              <div className="overlay"></div>
+              <div className="title-video-sub text-white p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+            </div>
+            <div className="flex box-image-video">
+              <img src="https://cdn.yeudulich.com/940x630/media/attraction/attraction/e5/df/47b9-4bea-4849-b339-f8c17c8aecfa.jpg" alt="" />
+              <div className="overlay"></div>
+              <div className="title-video-sub text-white p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+            </div>
+            <div className="flex box-image-video">
+              <img src="https://cdn.yeudulich.com/940x630/media/attraction/attraction/e5/df/47b9-4bea-4849-b339-f8c17c8aecfa.jpg" alt="" />
+              <div className="overlay"></div>
+              <div className="title-video-sub text-white p-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+            </div>
+          </div>
         </div>
       </div>
-      <style jsx>{`
-        
+      <style global jsx>{`
+        .video-main:focus {
+          outline: none;
+        }
+        .box-image-video {
+          padding-top: calc(2 / 3 * 100%);
+          position: relative;
+        }
+
+        .box-image-video img {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+        .box-image-video .overlay {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: linear-gradient(to bottom, rgba(59, 130, 246, 0), rgba(59, 130, 246, 1));
+        }
+        .title-video-sub {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+        }
       `}</style>
     </div>
   );
