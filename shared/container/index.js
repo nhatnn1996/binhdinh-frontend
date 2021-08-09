2031f9e8e93c397f1b431dc86e8b386616382320
nhatnn1996 <50484293+nhatnn1996@users.noreply.github.com>
2021-08-09 22:36:13 +0700

    Update index.js

--- shared/container/index.js
+++ shared/container/index.js
@@ -1,2 +1,2 @@
-export const url_api = "http://103.137.185.86:1337";
-export const url_base = "http://103.137.185.86:1337";
+export const url_api = "https://admin.nuocnongthon.binhdinh.vn";
+export const url_base = "https://admin.nuocnongthon.binhdinh.vn";
